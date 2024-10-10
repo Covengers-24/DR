5d0e3263c20677e93334245184a92f118b3522ea
KimSeungJin2024 <kimg0531@naver.com>
2024-10-10 15:23:50 +0900

    20241010 15:23 김승진 나만의레시피챗봇레시피 글쓰기 api 1차 수정

--- workspace/js/recipe/myDetailPage.js
+++ workspace/js/recipe/myDetailPage.js
@@ -7,3 +7,27 @@ heartImage.addEventListener('click', function() {
     heartImage.src = './../../image/heartGray.png'; // 검정 하트 이미지 경로
   }
 });
+
+document.getElementById('deleteButton').addEventListener('click', function () {
+  // 삭제 확인창 띄우기
+  if (confirm("정말로 삭제하시겠습니까?")) {
+    // 확인을 누르면 삭제 완료 알림
+    alert("삭제완료되었습니다.");
+    // 여기에서 실제 삭제 처리가 필요한 경우, 추가 코드를 작성합니다.
+  } else {
+    // 취소를 누르면 아무 동작 없이 원래 페이지로 유지
+    return;
+  }
+});
+
+document.getElementById('deleteButtons').addEventListener('click', function () {
+  // 삭제 확인창 띄우기
+  if (confirm("정말로 삭제하시겠습니까?")) {
+    // 확인을 누르면 삭제 완료 알림
+    alert("삭제완료되었습니다.");
+    // 여기에서 실제 삭제 처리가 필요한 경우, 추가 코드를 작성합니다.
+  } else {
+    // 취소를 누르면 아무 동작 없이 원래 페이지로 유지
+    return;
+  }
+});
