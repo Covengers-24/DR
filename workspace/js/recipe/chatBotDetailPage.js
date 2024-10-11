12630fc8af769f1fc2cb74a24c98c94add5c8e85
KimSeungJin2024 <kimg0531@naver.com>
2024-10-11 14:43:12 +0900

    20241011 14:43 김승진 레시피 상세페이지 완료

--- workspace/js/recipe/chatBotDetailPage.js
+++ workspace/js/recipe/chatBotDetailPage.js
@@ -8,3 +8,23 @@ heartImage.addEventListener('click', function() {
   }
 });
 
+document.getElementById('deleteButton').addEventListener('click', function () {
+  // 삭제 확인창 띄우기
+  if (confirm("정말로 삭제하시겠습니까?")) {
+    // 확인을 누르면 삭제 완료 알림
+    alert("삭제완료되었습니다.");
+    // 여기에서 실제 삭제 처리가 필요한 경우, 추가 코드를 작성합니다.
+  } else {
+    // 취소를 누르면 아무 동작 없이 원래 페이지로 유지
+    return;
+  }
+});
+
+function showEditComment() {
+  var commentInput = document.getElementById("commentInput");
+  if (commentInput.style.display === "none") {
+    commentInput.style.display = "block";
+  } else {
+    commentInput.style.display = "none";
+  }
+}
