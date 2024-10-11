d12719c0143a18519fe81ca0f6b88ff3e1c8334f
yanghyeoksu-24 <soe7759@naver.com>
2024-10-11 18:52:12 +0900

    241011 양혁수 이메일 찾기 기능 저장

--- workspace/js/user/emailFind.js
+++ workspace/js/user/emailFind.js
@@ -6,20 +6,3 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function() {
-  $('#sendCode').click(function() {
-    const phoneInput = $('#phone').val();
-    const phoneError = $('#phoneError');
-
-    // 정규 표현식을 사용하여 입력값 검증
-    const phonePattern = /^[0-9]{10,11}$/; // 10~11자리 숫자만 허용
-
-    // 입력값이 형식에 맞지 않을 경우
-    if (!phonePattern.test(phoneInput)) {
-      phoneError.text("형식에 맞게 입력해주세요.");
-    } else {
-      phoneError.text(""); // 오류 메시지 초기화
-      // 여기에 인증 요청을 위한 AJAX 호출 등을 추가할 수 있습니다.
-    }
-  });
-});
