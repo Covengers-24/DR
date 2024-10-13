dd3822a2da3c6a4bfe971ad8f6ad4180b8ff25c5
KimSeungJin2024 <kimg0531@naver.com>
2024-10-13 20:10:33 +0900

    20241013 20:10 김승진상세페이지 댓글수정부분 수정

--- workspace/js/recipe/chatBotDetailPage.js
+++ workspace/js/recipe/chatBotDetailPage.js
@@ -40,3 +40,53 @@ function showEditComment() {
     commentInput.style.display = "none";
   }
 }
+
+// 댓글 수정 버튼을 눌렀을 때 실행되는 함수
+function editComment(commentId) {
+  var commentText = document.getElementById('commentText' + commentId);
+  var editInput = document.getElementById('editInput' + commentId);
+  var editTextarea = document.getElementById('editTextarea' + commentId);
+  var buttonGroup = document.getElementById('buttonGroup' + commentId); // 수정/삭제 버튼 그룹
+
+  // 기존 댓글 텍스트를 textarea에 삽입
+  editTextarea.value = commentText.innerText;
+
+  // 댓글 텍스트를 숨기고 수정창을 보이게 함
+  commentText.style.display = 'none';
+  editInput.style.display = 'block';
+
+  // 수정/삭제 버튼 숨김
+  buttonGroup.style.display = 'none';
+}
+
+// 댓글 저장 버튼을 눌렀을 때 실행되는 함수
+function saveComment(commentId) {
+  var commentText = document.getElementById('commentText' + commentId);
+  var editInput = document.getElementById('editInput' + commentId);
+  var editTextarea = document.getElementById('editTextarea' + commentId);
+  var buttonGroup = document.getElementById('buttonGroup' + commentId); // 수정/삭제 버튼 그룹
+
+  // 수정된 내용을 댓글 텍스트에 적용
+  commentText.innerText = editTextarea.value;
+
+  // 수정창을 숨기고, 댓글 텍스트를 다시 보이게 함
+  editInput.style.display = 'none';
+  commentText.style.display = 'block';
+
+  // 수정/삭제 버튼 다시 보이게 함
+  buttonGroup.style.display = 'flex';
+}
+
+// 댓글 수정 취소 버튼을 눌렀을 때 실행되는 함수
+function cancelEdit(commentId) {
+  var commentText = document.getElementById('commentText' + commentId);
+  var editInput = document.getElementById('editInput' + commentId);
+  var buttonGroup = document.getElementById('buttonGroup' + commentId); // 수정/삭제 버튼 그룹
+
+  // 수정창을 숨기고, 댓글 텍스트를 다시 보이게 함
+  editInput.style.display = 'none';
+  commentText.style.display = 'block';
+
+  // 수정/삭제 버튼 다시 보이게 함
+  buttonGroup.style.display = 'flex';
+}
