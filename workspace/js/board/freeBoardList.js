3882e40fd2961e1593bad8665beb2daf0c5c3fe1
yanghyeoksu-24 <soe7759@naver.com>
2024-10-14 11:50:31 +0900

    241014 양혁수 게시판 목록 부분 최신순 버튼js저장

--- workspace/js/board/freeBoardList.js
+++ workspace/js/board/freeBoardList.js
@@ -21,8 +21,11 @@ $(function () {
   container.pagination('goToPage', 1);
 });
 
-// 최신순,추천순 버튼 액티브
+// 최신순, 추천순 버튼 액티브
 $(document).ready(function() {
+  // 페이지 로드 시 최신순 버튼에 active 클래스 추가
+  $('.freeboardlist-firstButton').addClass('active');
+  
   $('.freeboardlist-rightButton button').click(function() {
     // 모든 버튼에서 active 클래스 제거
     $('.freeboardlist-rightButton button').removeClass('active');
